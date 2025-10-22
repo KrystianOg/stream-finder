9ed7e7de0543c4886d137810e120f1a35c7a7d56
KrystianOg <krystian@ogonowski.it>
2025-10-22 22:00:08 +0200

    change streaming client to use baseurl variable
    it has to be composable now

--- frontend/app/utils/streaming-api.ts
+++
@@ -1,7 +0,0 @@
-import { Client, Configuration } from 'streaming-availability'
-
-export const sdk = new Client(
-    new Configuration({
-        basePath: 'http://localhost:3000/api/streaming-api',
-    })
-)
